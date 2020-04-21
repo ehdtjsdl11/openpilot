f83a822900b7f3a732fdd8882395aed0603ea0f1
opbykyd <opbykyd@gmail.com>
2020-04-22 02:28:21 +0900

    Update thermald.py

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -223,6 +223,9 @@ def thermald_thread():
   ts_last_charging_ctrl = None
   dp_last_modified = None
   ip_addr = '255.255.255.255'
+  
+  # sound trigger
+  sound_trigger = 1
 
   env = dict(os.environ)
   env['LD_LIBRARY_PATH'] = mediaplayer
@@ -411,9 +414,9 @@ def thermald_thread():
         off_ts = sec_since_boot()
         os.system('echo powersave > /sys/class/devfreq/soc:qcom,cpubw/governor')
 
-      if msg.thermal.batteryStatus == "Discharging" and started_seen and (sec_since_boot() - off_ts) == 3:
+      if sound_trigger == 1 and msg.thermal.batteryStatus == "Discharging" and started_seen and (sec_since_boot() - off_ts) > 3:
         subprocess.Popen([mediaplayer + 'mediaplayer', '/data/openpilot/selfdrive/assets/sounds/finish.wav'], shell = False, stdin=None, stdout=None, stderr=None, env = env, close_fds=True)
-
+        sound_trigger = 0
       # shutdown if the battery gets lower than 3%, it's discharging, we aren't running for
       # more than a minute but we were running
       if msg.thermal.batteryPercent < BATT_PERC_OFF and msg.thermal.batteryStatus == "Discharging" and \
